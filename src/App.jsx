4df41d99d93ce288fd1918af37549fffc69dfdfa
pje8740 <pje8740@cu.ac.kr>
2025-09-22 23:20:37 +0900

    jsx 수정 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,54 +7,32 @@ function App() {
   const [count, setCount] = useState(0);
 
   const incrementCount = () => {
-    setCount(count + 1);
+    setCount((prev) => prev + 1);
   };
 
   const decrementCount = () => {
-    setCount(count - 1);
+    setCount((prev) => prev - 1);
   };
 
+  /* 2. 상태로 관리하고 있는 count 값을 props를 통해 CountResult 컴포넌트로 전달하세요. */
   return (
     <>
       <h1>⏰ 카운트 앱 생성하기 </h1>
       <p>버튼을 누를 때 마다 버튼이 실시간으로 변경되도록 구현해 주세요.</p>
       <h2>count: {count}</h2>
-      <Count count={count} />
-      <PlusButton setCount={setCount} incrementCount={incrementCount} />
-      <MinusButton setCount={setCount} decrementCount={decrementCount} />
+      <CountResult count={count} />
+      <PlusButton incrementCount={incrementCount} />
+      <MinusButton decrementCount={decrementCount} />
     </>
   );
 }
 
-function PlusButton({ setCount, incrementCount }) {
-  return (
-    <button
-      onClick={() => {
-        setCount((prev) => prev + 1);
-      }}
-    >
-      {" "}
-      +{" "}
-    </button>
-  );
+function PlusButton({ incrementCount }) {
+  return <button onClick={incrementCount}>+</button>;
 }
 
-function MinusButton({ setCount }) {
-  return (
-    <button
-      onClick={() => {
-        setCount((prev) => prev - 1);
-      }}
-    >
-      {" "}
-      -{" "}
-    </button>
-  );
+function MinusButton({ decrementCount }) {
+  return <button onClick={decrementCount}>-</button>;
 }
 
-/* 2. 상태로 관리하고 있는 count 값을 props를 통해 CountResult 컴포넌트로 전달하세요. */
-function Count(props) {
-  console.log("props", props);
-  return;
-}
 export default App;
