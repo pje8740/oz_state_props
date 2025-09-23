c131aa0a908408ea43cf9bfa9c295b75e6b89943
pje8740 <pje8740@cu.ac.kr>
2025-09-23 10:49:08 +0900

    jsx 수정 3

--- src/CountResult.jsx
+++ src/CountResult.jsx
@@ -1,5 +1,5 @@
 // 3. props를 사용하여 count 값을 받아오세요.
-export default function CountResult() {
+export default function CountResult({ count }) {
   // 4. 받아온 count 횟수를 아래에 표기할 수 있도록 하세요.
-  return <p>{`☑️ 총 카운트 수는 회 입니다.`}</p>;
+  return <p>{`☑️ 총 카운트 수는 ${count}회 입니다.`}</p>;
 }
